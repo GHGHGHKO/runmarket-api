c0b76aaae76edf4b08fbac663dfcb7f5bfb09747
feelsgoodfrog <gudrb963@gmail.com>
2026-04-25 21:36:22 +0900

    test: add login, register, verify API unit, integration test code

--- src/test/java/com/runmarket/api/ApiApplicationTests.java
+++ src/test/java/com/runmarket/api/ApiApplicationTests.java
@@ -2,9 +2,15 @@ package com.runmarket.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import com.runmarket.api.adapter.out.email.EmailAdapter;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApiApplicationTests {
+
+    @MockitoBean EmailAdapter emailAdapter;
 
 	@Test
 	void contextLoads() {
